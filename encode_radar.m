dca9e867796edf41aeef105e8b2ed4248ad9d704
Kai <zhengkai28@hotmail.com>
2022-11-22 15:10:37 -0800

    Add files via upload
    LSM + SLAYER

---
+++ encode_radar.m
@@ -0,0 +1,100 @@
+%% Convert the spike timestamp file to SLAYER compatible format
+n_total = 2400;
+n_train = 1920;
+n_test = 480;
+% 
+% n_total = 10;
+% n_train = 8;
+% n_test = 2;
+
+% label information files
+train_file_name = "gesture_lsm001_slayer/train1920.txt";
+fout_train = fopen(train_file_name, 'w');
+
+test_file_name = "gesture_lsm001_slayer/test480.txt";
+fout_test = fopen(test_file_name, 'w');
+
+fprintf(fout_train, "#sample\t#class\n");
+fprintf(fout_test, "#sample\t#class\n");
+
+% timestamp files, training
+for ii = 1:n_train
+    in_file_name = sprintf("gesture_lsm_001/gesture_%d.txt", ii-1);
+    out_file_name = sprintf("gesture_lsm001_slayer/%d.bs1", ii);
+    fin = fopen(in_file_name, 'r');
+    
+    label = str2num(fgetl(fin)) - 1;
+    
+    neuronID = [];
+    timeID = [];
+    polarity = [];
+    
+    while ~feof(fin)
+        line = fgetl(fin);
+    
+        x = str2num(line);
+        id = round(x(1) / 2);  % (1, 2), (3, 4), (5, 6), ...
+        time = x(2);
+        pol = mod(x(1), 2);       % 1, 3, 5 positive; 2, 4, 6 negative. 
+        
+        neuronID = [neuronID; id];
+        timeID = [timeID; time];
+        polarity = [polarity; pol];
+    
+        %fprintf("%d %d %0.4f\n", id, pol, time);
+    end
+        
+    fclose(fin);
+
+    % write the label information file. Label starts from 0. 
+    fprintf(fout_train, "%d\t%d\n", ii, label);
+    
+    % neuronID starts from 1.  { neuronID -= 1 } in the encoding function. 
+    eventStamp = [neuronID, timeID, polarity];
+    
+    % encodeSpikes(<name fo spike data file>, <eventStamp = [neuronID, timeID(ms), polarity]>)
+    encode1DBinSpikes(out_file_name, eventStamp);
+end
+
+fclose(fout_train);
+
+% timestamp files, testing
+for ii = (n_train + 1):n_total
+    in_file_name = sprintf("gesture_lsm_001/gesture_%d.txt", ii-1);
+    out_file_name = sprintf("gesture_lsm001_slayer/%d.bs1", ii);
+    fin = fopen(in_file_name, 'r');
+
+    % from 0 to 11. total 12 labels. 
+    label = str2num(fgetl(fin)) - 1;
+    
+    neuronID = [];
+    timeID = [];
+    polarity = [];
+    
+    while ~feof(fin)
+        line = fgetl(fin);
+    
+        x = str2num(line);
+        id = round(x(1) / 2);  % (1, 2), (3, 4), (5, 6), ...
+        time = x(2);
+        pol = mod(x(1), 2);       % 1, 3, 5 positive; 2, 4, 6 negative. 
+        
+        neuronID = [neuronID; id];
+        timeID = [timeID; time];
+        polarity = [polarity; pol];
+    
+        %fprintf("%d %d %0.4f\n", id, pol, time);
+    end
+        
+    fclose(fin);
+
+    % write the label information file. Label starts from 0. 
+    fprintf(fout_test, "%d\t%d\n", ii, label);
+    
+    eventStamp = [neuronID, timeID, polarity];
+    
+    % encodeSpikes(<name fo spike data file>, <eventStamp = [neuronID, timeID(ms), polarity]>)
+    encode1DBinSpikes(out_file_name, eventStamp);
+end
+
+fclose(fout_test);
---
+++ lsm_sparse_to_file.py
@@ -0,0 +1,164 @@
+#! /usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import rand
+from numpy.random import randint
+from scipy.linalg import eig
+
+import nengo
+import nengo_dl
+from nengo.utils.matplotlib import rasterplot
+from nengo.dists import Uniform
+#from nengo.processes import Piecewise
+
+dt = 0.001
+t_sim = 1.5
+total_pts = int(t_sim / dt)
+
+ens_seed = 1
+np_seed = 1
+np.random.seed(np_seed)
+rng = np.random.RandomState(np_seed)
+
+class InputFunc:
+    def __init__(self, filename):
+        self.length = 1.5
+        self.nchans = 6
+        self.nsamps = 2400
+        self.npts = 1500
+        self.total_pts = total_pts
+
+        # valid data points 1500, add 1500 zeros following that
+        self.xdata = np.zeros((self.nsamps, self.total_pts, self.nchans))
+        self.ydata = np.zeros((self.nsamps))
+
+        self.samp_id = 0
+
+        with open(filename, "r")  as in_file:
+            data_lines = in_file.readlines()
+        
+        n_signal = self.nchans * self.npts
+        for i in range(self.nsamps):
+            line = data_lines[i]
+            items = np.array(line.split(" "))
+            items = items.astype(int) 
+            self.ydata[i] = items[n_signal]    # 0-11 for sparse_catogorial_enthropy
+            #print(str(int(self.ydata[i])))
+            items = items * 1000                # scale by 1000
+            self.xdata[i, :self.npts, :] = np.reshape(items[:n_signal], (self.nchans, self.npts)).T
+
+        perm = rng.permutation(self.xdata.shape[0])
+        self.xdata = self.xdata[perm, :]
+        self.ydata = self.ydata[perm]
+        return  
+
+    def set_id(self, samp_id):
+        self.samp_id = samp_id
+        return 0
+
+    def step(self, t):
+        ii = int(t / dt - dt)
+        if ii >= self.npts:
+            return np.zeros(self.nchans)
+        else:
+            return self.xdata[self.samp_id, ii, :]
+
+my_input = InputFunc("gesture_2400_1ms.txt")
+my_input.set_id(1000)
+
+model = nengo.Network(label="lsm")
+with model:
+    n_neurons = 256
+    n_inputs = 6
+    n_outputs = 12
+    # The ensemble for the oscillator
+    pool = nengo.Ensemble(
+        n_neurons, 
+        dimensions=1, 
+        neuron_type = nengo.LIF(),
+        intercepts=Uniform(0, 0),
+        max_rates=Uniform(100, 100),
+        seed=ens_seed
+    )
+
+    inp = nengo.Node(my_input.step)
+    #inp = nengo.Node(np.zeros(6))
+    #outp = nengo.Node(size_in=n_outputs)
+    
+    gain = 1/100 # normalize by average firing rate
+    in_density = 0.4
+    lsm_density = 0.4
+    n_in_conn = int(in_density * n_neurons)
+    n_lsm_conn = int(lsm_density * n_neurons) 
+    # initialize input connection to the reservoir
+    w_in = np.zeros((n_neurons, n_inputs))
+    for i in range(int(n_inputs/2)):
+        for j in range(n_in_conn):
+            weight = rand() - 0.5
+            neuron_sel = randint(0, n_neurons - 1)
+            w_in[neuron_sel, 2 * i] = weight
+            w_in[neuron_sel, 2 * i + 1] = -weight
+    
+    #w_bias = rand(n_neurons, 1) - 0.5
+    w_bias = np.zeros((n_neurons, 1))
+    
+    w_pool = np.zeros((n_neurons, n_neurons))
+    for i in range(n_neurons):
+        for j in range(n_lsm_conn):
+            w_pool[randint(0, n_neurons - 1), i] = rand() - 0.5
+
+    #w_pool = rand(n_neurons, n_neurons) - 0.5
+    coeff = gain / max(abs(eig(w_pool)[0]))
+    print("coeff = %f" % coeff)
+    w_pool *= coeff
+
+    # initialize the readout layer connection
+    #w_out = rand(n_outputs, n_neurons) - 0.5
+    
+    nengo.Connection(inp, pool.neurons, transform=w_in)
+    nengo.Connection(pool.neurons, pool.neurons, transform=w_pool, synapse=0.1)
+    #nengo.Connection(pool.neurons, outp, transform=w_out)
+
+    # Indicate which values to record
+    in_probe = nengo.Probe(inp, synapse=0)
+    pool_neuron_probe = nengo.Probe(pool.neurons)
+    #out_probe_filt = nengo.Probe(outp, synapse=0.03)
+    #out_probe = nengo.Probe(outp, synapse=0)
+
+write_file = True
+batch_size = 40
+for i in range(60):
+    print("Batch #%d." % i)
+    with nengo_dl.Simulator(model, minibatch_size=batch_size) as sim:
+    # batch processing. shape: (minibatch_size, n_steps, node_size)
+        sim.run(t_sim, data={inp:my_input.xdata[batch_size*i:batch_size*(i+1), :]})
+        print(sim.data[pool_neuron_probe].shape)
+
+        if write_file:
+            for j in range(batch_size):
+                samp = batch_size * i + j 
+                print("Sample #%d" % samp)
+                filename = "gesture_lsm_002/gesture_" + str(samp) + ".txt"
+                with open(filename, "w") as out_file:
+                    out_file.write(str(int(my_input.ydata[samp])) + "\n")   # line 1 for label. 
+                    dat = sim.data[pool_neuron_probe][j]
+                    print(dat.shape)
+                    for k in range(total_pts): 
+                        ts = k / 1000.0
+                        for l in range(n_neurons): 
+                            if dat[k, l] > 0.0:
+                                out_file.write(str(l) + ' ' + str(ts) +"\n")
+
+# plot sample output. label wrong for batch_size > 1
+for i in range(6):
+    plt.figure()
+    rasterplot(sim.trange(), sim.data[in_probe][i])
+    plt.title("%d" % int(my_input.ydata[i]))
+    plt.xlabel("Time (s)")
+
+    plt.figure()
+    rasterplot(sim.trange(), sim.data[pool_neuron_probe][i])
+    plt.title("%d" % int(my_input.ydata[i]))
+    plt.xlabel("Time (s)")
+plt.show()
